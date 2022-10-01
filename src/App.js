cae5e97ccc7d30d8ec2fb5cbc290b35f062c196c
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-30 23:13:41 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Create Something</h1>
+        <div className="input">
+          <input></input>
+          <button>Add Task<h1>Create</h1></button>
+        </div>
       </header>
     </div>
   );
