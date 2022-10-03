9d36d4fdec80b42d8fb6bc604ece6b62bcfcbafb
LeoKnox <LeoKnox@users.noreply.github.com>
2022-10-02 22:33:34 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-import NewList from './list.js';
-import Red from './red.js';
-import Blue from './blue.js';
 import { useState } from "react";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [newTask, setTask] = useState("");
-  const [comp, setComp] = useState(false);
-  const [color, setColor] = useState("Red");
 
   const handleChange = (e) => {
     setTask(e.target.value);
@@ -16,10 +11,6 @@ function App() {
 
   const addTask = () => {
     setTodoList([...todoList, newTask]);
-  }
-
-  const changeColor = () => {
-    setComp(!comp);
   }
 
   return (
@@ -30,11 +21,16 @@ function App() {
           <input onChange={handleChange} />
           <button onClick={addTask}>Add Task</button>
         </div>
-        <NewList iList={todoList} />
-        { comp ? <Red /> : <Blue />}
-        <button onClick={changeColor}>Change</button>
-        <button onClick={() => setComp(true)}>Red</button>
-        <button onClick={() => setComp(false)}>Blue</button>
+        <table>
+            {todoList.map ((task, index) => (
+                <tr>
+                    <td>{task}</td>
+                    <td>
+                        <button>Delete:{index}</button>
+                    </td>
+                </tr>
+            ))}
+        </table>
       </header>
     </div>
   );
