8d65d3644c6c347a778036609963d2be02c978e6
LeoKnox <LeoKnox@users.noreply.github.com>
2022-11-04 21:33:53 -0700

    Update Rooms.js

--- rooms/Rooms.js
+++ rooms/Rooms.js
@@ -1,7 +1,10 @@
 export default function Room() {
+  const rooms = [
+    {id:1, name: "entry"}
+  ]
   return (
     <>
-    <h1>show rooms here</h1>
+    <h1>show rooms here {rooms[0].name}</h1>
     </>
   );
 };
