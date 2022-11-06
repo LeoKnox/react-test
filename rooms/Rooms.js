5b110a2c38d3de491d5604320c2f0cb015f94ec5
LeoKnox <LeoKnox@users.noreply.github.com>
2022-11-05 23:35:07 -0700

    Update Rooms.js

--- rooms/Rooms.js
+++ rooms/Rooms.js
@@ -1,6 +1,6 @@
 export default function Room() {
   const rooms = [
-    {id:1, name: "entry", x:0, y:0, width: 5, height: 5},
+    {id:1, name: "entry", x:1, y:1, width: 5, height: 5},
     {id:2, name: "second", x:6, y:4, width: 7, height: 4}
   ]
   return (
@@ -9,7 +9,7 @@ export default function Room() {
     {rooms.map(room => (
       <>
       <p>{room.name}</p>
-      <rect x={9*room.x} y={9*room.y} width={11*room.width} height={11*room.height} fill="blue"/>
+      <rect x={9*room.x} y={9*room.y} width={12*room.width} height={12*room.height} fill="blue"/>
       <rect x={10*room.x} y={10*room.y} width={10*room.width} height={10*room.height} fill="red"/>
       </>
     ))}
