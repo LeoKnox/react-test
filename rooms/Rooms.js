4d2ebe01eacb5bc551afab332c1d706a9737b2e5
LeoKnox <LeoKnox@users.noreply.github.com>
2022-11-05 21:54:31 -0700

    Update Rooms.js

--- rooms/Rooms.js
+++ rooms/Rooms.js
@@ -1,6 +1,7 @@
 export default function Room() {
   const rooms = [
-    {id:1, name: "entry", x:1, y:1, width: 5, height: 5}
+    {id:1, name: "entry", x:0, y:0, width: 5, height: 5},
+    {id:2, name: "second", x:6, y:4, width: 7, height: 4}
   ]
   return (
     <>
@@ -8,7 +9,7 @@ export default function Room() {
     {rooms.map(room => (
       <>
       <p>{room.name}</p>
-      <rect x={room.x} y={room.y} width={60*room.width} height={60*room.width} fill="red"/>
+      <rect x={10*room.x} y={10*room.y} width={10*room.width} height={10*room.height} fill="red"/>
       </>
     ))}
     </svg>
