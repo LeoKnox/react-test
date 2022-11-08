622ca2ee6139c8fbc20484322cf3ef1708c1a3ea
LeoKnox <LeoKnox@users.noreply.github.com>
2022-11-07 23:36:31 -0800

    Update Rooms.js

--- rooms/Rooms.js
+++ rooms/Rooms.js
@@ -1,20 +1,20 @@
 export default function Room() {
   const rooms = [
     {id:1, name: "entry", x:2, y:2, width: 6, height: 5},
-    {id:2, name: "second", x:6, y:4, width: 7, height: 4}
+    {id:2, name: "second", x:12, y:4, width: 7, height: 4}
   ]
 
   function Box(test) {
     console.log(test);
     return(
       <rect
-        x={20*test.test.x}
-        y={20*test.test.y}
+        x={10*test.test.x}
+        y={10*test.test.y}
         width={10*test.test.width}
         height={10*test.test.height}
         fill="gray"
-        stroke-width="3"
-        stroke="darkgray"
+        stroke-width="10"
+        stroke="brown"
       />
     );
   }
