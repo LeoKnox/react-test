2a401e5770385cd4426caa21ed25c35e6cc13b44
LeoKnox <LeoKnox@users.noreply.github.com>
2022-11-08 23:08:20 -0800

    Create rooms2.js

---
+++ rooms/rooms2.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Room from "./Rooms.js";
+import Rooms2 from "./Rooms2.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Rooms2 />
+      <Room />
+    </div>
+  );
+}
