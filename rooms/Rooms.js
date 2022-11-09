ec9475f0f1474f1facb3a832ad6260c3c0867998
LeoKnox <LeoKnox@users.noreply.github.com>
2022-11-08 22:56:19 -0800

    Update Rooms.js

--- rooms/Rooms.js
+++ rooms/Rooms.js
@@ -1,5 +1,7 @@
+import {useState} from "react";
+
 export default function Room() {
-  const base = 20;
+  const [base, setBase] = useState(20);
   const rooms = [
     {id:1, name: "entry", x:2, y:2, width: 6, height: 5},
     {id:2, name: "second", x:12, y:4, width: 7, height: 4}
