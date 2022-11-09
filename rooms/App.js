bc3f2f5c1ee8fa87be73fcda80a32fb2f336e6d2
LeoKnox <LeoKnox@users.noreply.github.com>
2022-11-08 23:06:43 -0800

    Update App.js

--- rooms/App.js
+++ rooms/App.js
@@ -1,9 +1,11 @@
 import "./styles.css";
-import Room from './Rooms.js';
+import Room from "./Rooms.js";
+import Rooms2 from "./Rooms2.js";
 
 export default function App() {
   return (
     <div className="App">
+      <Rooms2 />
       <Room />
     </div>
   );
