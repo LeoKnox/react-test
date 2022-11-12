94a5c8a18117756ab8fa19898ed948bb42be0020
LeoKnox <LeoKnox@users.noreply.github.com>
2022-11-11 21:04:59 -0800

    Update Rooms3.js

--- rooms/Rooms3.js
+++ rooms/Rooms3.js
@@ -7,14 +7,9 @@ export default function Rooms3(rooms) {
       <>
       <p>{room.name}</p>
       <>
-      {(() => {(for(let i = 0; i < room.length; i++) {
-          <div>
-            <p>*</p>
-          </div>
-        }}
-        </>
-      </>
-    )))})}
+      {for (let i = 0; i < 10; i++) {
+        <p>{i}</p>
+      }}
     </>
   )
 }
