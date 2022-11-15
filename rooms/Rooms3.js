78e50f03ac64a022509e8e3f8e55d9607aec83a8
LeoKnox <LeoKnox@users.noreply.github.com>
2022-11-14 20:25:12 -0800

    Update Rooms3.js

--- rooms/Rooms3.js
+++ rooms/Rooms3.js
@@ -32,7 +32,7 @@ export default function Rooms3(rooms) {
     return myMap;
   }
 
-  function buttonClick(e) {
+  const buttonClick = (e) => {
     e.preventDefault();
     let newGrid = grid;
     //newGrid[0,0] = "+";
