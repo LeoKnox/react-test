3e73c7227be048f44b939900f7e76d56de2bf326
LeoKnox <LeoKnox@users.noreply.github.com>
2022-11-18 22:57:56 -0800

    Update abc.js

--- rooms/abc.js
+++ rooms/abc.js
@@ -6,8 +6,16 @@ export default function App() {
   useEffect(() => {
     setRed([1,2]);
   }, [red]);
+
+  function changeRed() {
+    let x = red;
+    x[1] = 3;
+    setRed(x);
+    console.log(red);
+  }
   return (
     <div className="App">
+      <button onClick={changeRed}>click</button>
       {red.map(g => (
         <p>{g}</p>
       ))}
