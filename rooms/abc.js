5e1791a09fbd6bd290b9b0b94f2ef4b0e1820aad
LeoKnox <LeoKnox@users.noreply.github.com>
2022-11-20 22:19:51 -0800

    Update abc.js

--- rooms/abc.js
+++ rooms/abc.js
@@ -9,7 +9,40 @@ function App() {
   const data = [1,2];
 
   useEffect(() => {
+    setA(data);import './App.css';
+import {useEffect, useState} from 'react';
+
+function App() {
+  const [a, setA] = useState([]);
+  const data = [1,2];
+
+  useEffect(() => {
     setA(data);
+  },[a]);
+  
+  function clickButton() {
+    console.log("dddd");
+    let newA = a;
+    newA[1] = 5;
+    setA(newA);
+    console.log("***"+a);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Heading</h1>
+        <button onClick={clickButton}>click</button>
+        {a.map((letter) => (
+          <p>{letter}</p>
+        ))}
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
   },[a]);
   
   function clickButton() {
